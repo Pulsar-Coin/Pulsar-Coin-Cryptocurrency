d012cc68bd89910f88de11dba362946e1b654731
Pulsar <98483344+PulsarCoin@users.noreply.github.com>
2023-07-23 19:39:24 -0500

    Update and rename bitcoinaddressvalidator.h to pulsaraddressvalidator.h

--- src/qt/pulsaraddressvalidator.h
+++ src/qt/pulsaraddressvalidator.h
@@ -1,35 +1,36 @@
 // Copyright (c) 2011-2014 The Bitcoin Core developers
+// Maintained and Managed by The Pulsar Coin Team
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_QT_BITCOINADDRESSVALIDATOR_H
-#define BITCOIN_QT_BITCOINADDRESSVALIDATOR_H
+#ifndef PULSAR_QT_PULSARADDRESSVALIDATOR_H
+#define PULSAR_QT_PULSARADDRESSVALIDATOR_H
 
 #include <QValidator>
 
 /** Base58 entry widget validator, checks for valid characters and
  * removes some whitespace.
  */
-class BitcoinAddressEntryValidator : public QValidator
+class PulsarAddressEntryValidator : public QValidator
 {
     Q_OBJECT
 
 public:
-    explicit BitcoinAddressEntryValidator(QObject *parent);
+    explicit PulsarAddressEntryValidator(QObject *parent);
 
     State validate(QString &input, int &pos) const;
 };
 
-/** Bitcoin address widget validator, checks for a valid bitcoin address.
+/** Pulsar address widget validator, checks for a valid Pulsar address.
  */
-class BitcoinAddressCheckValidator : public QValidator
+class PulsarAddressCheckValidator : public QValidator
 {
     Q_OBJECT
 
 public:
-    explicit BitcoinAddressCheckValidator(QObject *parent);
+    explicit PulsarAddressCheckValidator(QObject *parent);
 
     State validate(QString &input, int &pos) const;
 };
 
-#endif // BITCOIN_QT_BITCOINADDRESSVALIDATOR_H
+#endif // PULSAR_QT_PULSARADDRESSVALIDATOR_H
