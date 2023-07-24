772b87aa26d12d85da56025908e79a7e206f5b96
Pulsar <98483344+PulsarCoin@users.noreply.github.com>
2023-07-23 23:01:23 -0500

    Update utilitydialog.h

--- src/qt/utilitydialog.h
+++ src/qt/utilitydialog.h
@@ -1,14 +1,15 @@
 // Copyright (c) 2011-2017 The Bitcoin Core developers
+// Maintained and Managed by The Pulsar Coin Team
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_QT_UTILITYDIALOG_H
-#define BITCOIN_QT_UTILITYDIALOG_H
+#ifndef PULSAR_QT_UTILITYDIALOG_H
+#define PULSAR_QT_UTILITYDIALOG_H
 
 #include <QDialog>
 #include <QObject>
 
-class BitcoinGUI;
+class PulsarGUI;
 
 namespace Ui {
     class HelpMessageDialog;
@@ -42,10 +43,10 @@ class ShutdownWindow : public QWidget
 
 public:
     explicit ShutdownWindow(QWidget *parent=0, Qt::WindowFlags f=0);
-    static QWidget *showShutdownWindow(BitcoinGUI *window);
+    static QWidget *showShutdownWindow(PulsarGUI *window);
 
 protected:
     void closeEvent(QCloseEvent *event);
 };
 
-#endif // BITCOIN_QT_UTILITYDIALOG_H
+#endif // PULSAR_QT_UTILITYDIALOG_H
