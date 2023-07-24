dbb71953130977202a20af51c5a0c0c84b410fe3
Pulsar <98483344+PulsarCoin@users.noreply.github.com>
2023-07-23 19:22:32 -0500

    Update and rename bench_bitcoin.cpp to bench_pulsar.cpp

--- src/bench/bench_pulsar.cpp
+++ src/bench/bench_pulsar.cpp
@@ -1,4 +1,5 @@
 // Copyright (c) 2015-2017 The Bitcoin Core developers
+// Managed and Maintained by the Pulsar Coin Team
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
