efd033ec1aa3635069ddb23fea830eca5543b5c3
Jason <jason_hilton@hotmail.co.uk>
2023-08-23 15:59:58 +0100

    Update txdb.cpp

--- src/txdb.cpp
+++ src/txdb.cpp
@@ -259,7 +259,7 @@ bool CBlockTreeDB::ReadFlag(const std::string &name, bool &fValue) {
     return true;
 }
 
-bool CBlockTreeDB::LoadBlockIndexGuts(const Consensus::Params& consensusParams, std::function<CBlockIndex*(const uint256&)> insertBlockIndex)
+bool CBlockTreeDB::LoadBlockIndexGuts(const Consensus::Params& consensusParams, std::function<CBlockIndex*(const uint256&)> insertBlockIndex, int& nHighest)
 {
     std::unique_ptr<CDBIterator> pcursor(NewIterator());
 
