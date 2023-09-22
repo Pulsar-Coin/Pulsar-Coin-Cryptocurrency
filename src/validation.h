5f848fa560a18bca345c4d2dc042995e734bb3be
Jason <jason_hilton@hotmail.co.uk>
2023-09-22 23:41:27 +0100

    Update validation.h

--- src/validation.h
+++ src/validation.h
@@ -117,6 +117,8 @@ static const int64_t BLOCK_DOWNLOAD_TIMEOUT_BASE = 1000000;
 static const int64_t BLOCK_DOWNLOAD_TIMEOUT_PER_PEER = 500000;
 
 bool IsMinoEnabled(const CBlockIndex* pindexPrev, const Consensus::Params& params);
+bool IsHalvingActive(const CBlockIndex* pindexPrev, const Consensus::Params& params);
+
 
 static const int64_t DEFAULT_MAX_TIP_AGE = 24 * 60 * 60;
 /** Maximum age of our tip in seconds for us to be considered current for fee estimation */
